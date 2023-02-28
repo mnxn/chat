5c84ece2508047a23ee21d69bba1d93b96185cc8
Max Lantas <mnxndev@outlook.com>
2023-02-28 09:58:58 -0800

    server response types

---
+++ protocol/server.go
@@ -0,0 +1,60 @@
+package protocol
+
+type ResponseType uint32
+
+const (
+	Error ResponseType = iota
+	FatalError
+	RoomList
+	UserList
+	RoomMessage
+	UserMessage
+)
+
+type ErrorType uint32
+
+const (
+	Disconnection ErrorType = iota
+	InternalError
+	MalformedRequest
+	UnsupportedVersion
+	MissingRoom
+	MissingUser
+	ExistingRoom
+	ExistingUser
+	InvalidRoom
+	InvalidUser
+	InvalidText
+)
+
+type ErrorResponse struct {
+	Error ErrorType
+	Info  string
+}
+
+type FatalErrorResponse struct {
+	Error ErrorType
+	Info  string
+}
+
+type RoomListResponse struct {
+	Count uint32
+	Rooms []string
+}
+
+type UserListResponse struct {
+	Room  string
+	Count uint32
+	Users []string
+}
+
+type RoomMessageResponse struct {
+	Room   string
+	Sender string
+	Text   string
+}
+
+type UserMessageResponse struct {
+	Sender string
+	Text   string
+}
