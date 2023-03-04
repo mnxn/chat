b7e5ad9121c4c3c9a64b9d48a0a9838daeb77907
Max Lantas <mnxndev@outlook.com>
2023-03-04 12:21:00 -0800

    adjust string error messages

--- protocol/string.go
+++ protocol/string.go
@@ -13,12 +13,12 @@ func encodeString(w io.Writer, s string) error {
 
 	err := binary.Write(w, byteOrder, int32(len(bytes)))
 	if err != nil {
-		return fmt.Errorf("encode StringData: %w", err)
+		return fmt.Errorf("encode StringData.Length: %w", err)
 	}
 
 	err = binary.Write(w, byteOrder, bytes)
 	if err != nil {
-		return fmt.Errorf("encode StringData: %w", err)
+		return fmt.Errorf("encode StringData.Data: %w", err)
 	}
 
 	return nil
@@ -28,13 +28,13 @@ func decodeString(r io.Reader, s *string) error {
 	var length uint32
 	err := binary.Read(r, byteOrder, &length)
 	if err != nil {
-		return fmt.Errorf("decode StringData.length: %w", err)
+		return fmt.Errorf("decode StringData.Length: %w", err)
 	}
 
 	bytes := make([]byte, length)
 	err = binary.Read(r, byteOrder, &bytes)
 	if err != nil {
-		return fmt.Errorf("decode StringData.data: %w", err)
+		return fmt.Errorf("decode StringData.Data: %w", err)
 	}
 
 	*s = string(bytes)
