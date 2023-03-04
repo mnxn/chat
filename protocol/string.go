42dbde940a3b37304fad92dde4818f4b52ed5552
Max Lantas <mnxndev@outlook.com>
2023-03-04 14:45:11 -0800

    check for invalid UTF-8 strings

--- protocol/string.go
+++ protocol/string.go
@@ -2,9 +2,13 @@ package protocol
 
 import (
 	"encoding/binary"
+	"errors"
 	"fmt"
 	"io"
+	"unicode/utf8"
 )
+
+var ErrInvalidUtf8String = errors.New("invalid UTF-8 string")
 
 var byteOrder = binary.BigEndian
 
@@ -14,6 +18,10 @@ func encodeString(w io.Writer, s string) error {
 	err := binary.Write(w, byteOrder, uint32(len(bytes)))
 	if err != nil {
 		return fmt.Errorf("encode StringData.Length: %w", err)
+	}
+
+	if !utf8.ValidString(s) {
+		return fmt.Errorf("encode StringData.Data: %w", ErrInvalidUtf8String)
 	}
 
 	err = binary.Write(w, byteOrder, bytes)
@@ -35,6 +43,10 @@ func decodeString(r io.Reader, s *string) error {
 	err = binary.Read(r, byteOrder, &bytes)
 	if err != nil {
 		return fmt.Errorf("decode StringData.Data: %w", err)
+	}
+
+	if !utf8.Valid(bytes) {
+		return fmt.Errorf("decode StringData.Data: %w", ErrInvalidUtf8String)
 	}
 
 	*s = string(bytes)
