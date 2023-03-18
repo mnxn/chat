3090259021af1bc06ce28dd43bdcc62ab93bb682
Max Lantas <mnxndev@outlook.com>
2023-03-17 18:49:06 -0700

    client: parse name and flags

--- bin/client/main.go
+++ bin/client/main.go
@@ -1,13 +1,44 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
+	"os"
+	"path/filepath"
 
 	"github.com/mnxn/chat/client"
 )
 
+var (
+	host = flag.String("host", "localhost", "chat server hostname")
+	port = flag.Int("port", 5555, "chat server port number")
+)
+
 func main() {
-	c := client.NewClient("me", "localhost", 5555)
+	flag.Usage = func() {
+		exe := filepath.Base(os.Args[0])
+		fmt.Fprintf(flag.CommandLine.Output(), "usage: %s [options] name\noptions:\n", exe)
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+
+	fmt.Printf("connecting to %s:%d\n", *host, *port)
+
+	var name string
+	if len(os.Args) > 1 {
+		name = os.Args[1]
+	} else {
+		fmt.Print("enter display name: ")
+		scanner := bufio.NewScanner(os.Stdin)
+		if !scanner.Scan() {
+			fmt.Fprintln(os.Stderr, "error reading name.")
+			return
+		}
+		name = scanner.Text()
+	}
+
+	c := client.NewClient(name, *host, *port)
 	err := c.Run()
 	if err != nil {
 		fmt.Printf("client error: %s", err.Error())
