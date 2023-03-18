5846c67cb506ef6fe243fc3337b30fe457de1c94
Max Lantas <mnxndev@outlook.com>
2023-03-17 17:30:23 -0700

    client: move parse function to new file

--- client/client.go
+++ client/client.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"net"
 	"os"
-	"strings"
 
 	"github.com/mnxn/chat/protocol"
 )
@@ -99,102 +98,5 @@ func (c *Client) Run() error {
 		case <-c.done:
 			return nil
 		}
-	}
-}
-
-func (c *Client) parse(input string) {
-	if !strings.HasPrefix(input, "/") {
-		c.outgoing <- &protocol.MessageRoomRequest{
-			Room: "",
-			Text: input,
-		}
-		return
-	}
-
-	split := strings.SplitN(input[1:], " ", 3)
-	if len(split) < 1 {
-		c.output <- "[command error] invalid command: use /help to see all commands"
-		return
-	}
-
-	switch split[0] {
-	default:
-		c.output <- "[command error] invalid command: use /help to see all commands"
-
-	case "help":
-		c.output <- `command help:
-    /help             show this message
-    /rooms            list rooms in the server
-    /users            list users in the server
-    /users  [room]    list users in a room
-    /msg    [room]    send a message to a specific room
-    /dm     [user]    send a direct message to a user
-    /create [room]    create a room
-    /join   [room]    join a room
-    /leave  [room]    leave a room
-    /quit             quit the chat program`
-
-	case "rooms":
-		c.outgoing <- &protocol.ListRoomsRequest{}
-
-	case "users":
-		var room string
-		if len(split) >= 2 {
-			room = split[1]
-		}
-		c.outgoing <- &protocol.ListUsersRequest{
-			Room: room,
-		}
-
-	case "msg":
-		if len(split) < 2 {
-			c.output <- "[command error] missing command arguments: use /help to see usage"
-			return
-		}
-		c.outgoing <- &protocol.MessageRoomRequest{
-			Room: split[1],
-			Text: split[2],
-		}
-
-	case "dm":
-		if len(split) < 2 {
-			c.output <- "[command error] missing command arguments: use /help to see usage"
-			return
-		}
-		c.outgoing <- &protocol.MessageUserRequest{
-			User: split[1],
-			Text: split[2],
-		}
-
-	case "create":
-		if len(split) < 2 {
-			c.output <- "[command error] missing command argument: use /help to see usage"
-			return
-		}
-		c.outgoing <- &protocol.CreateRoomRequest{
-			Room: split[1],
-		}
-
-	case "join":
-		if len(split) < 2 {
-			c.output <- "[command error] missing command argument: use /help to see usage"
-			return
-		}
-		c.outgoing <- &protocol.JoinRoomRequest{
-			Room: split[1],
-		}
-
-	case "leave":
-		if len(split) < 2 {
-			c.output <- "[command error] missing command argument: use /help to see usage"
-			return
-		}
-		c.outgoing <- &protocol.LeaveRoomRequest{
-			Room: split[1],
-		}
-
-	case "quit":
-		c.outgoing <- &protocol.DisconnectRequest{}
-		c.done <- struct{}{}
 	}
 }
